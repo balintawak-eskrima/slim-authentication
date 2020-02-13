53a0d0a9b361e73e7d214d444d2a57d232d76102
Bogdan Titomir <bogdan.titomir@outlook.com>
2020-02-13 11:10:03 +0500

    foundation disposition

---
+++ tests/FooTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class FooTest extends TestCase
+{
+    public function testBar()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
